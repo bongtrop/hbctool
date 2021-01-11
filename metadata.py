a61236ad2087a83dedea8c44b0fe1fea33547e06
Pongsakorn Sommalai <bongtrop@gmail.com>
2021-01-11 17:59:55 +0700

    Release version 0.1.1

--- metadata.py
+++ metadata.py
@@ -1,7 +1,7 @@
 package = 'hbctool'
 project = 'hbctool'
 project_no_spaces = project.replace(' ', '')
-version = '0.1.0'
+version = '0.1.1'
 description = ('A command-line interface for disassembling and assembling'
                'the Hermes Bytecode')
 authors = ['bongtrop']
