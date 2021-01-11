5305a867c2fe41155db675aa6b2e31f7ae4801bd
Pongsakorn Sommalai <bongtrop@gmail.com>
2021-01-11 17:01:42 +0700

    Add commandline feature

--- hbctool.py
+++ hbctool.py
@@ -1,16 +1,73 @@
-import hbc as hbcl
+"""
+A command-line interface for disassembling and assembling
+the Hermes Bytecode.
+
+Usage:
+    hbctool disasm <HBC_FILE> <HASM_PATH>
+    hbctool asm <HASM_PATH> <HBC_FILE>
+    hbctool --help
+    hbctool --version
+
+Operation:
+    disasm              Disassemble Hermes Bytecode
+    asm                 Assemble Hermes Bytecode
+
+Args:
+    HBC_FILE            Target HBC file
+    HASM_PATH           Target HASM directory path
+
+Options:
+    --version           Show hbctool version
+    --help              Show hbctool help manual
+
+Examples:
+    hbctool disasm index.android.bundle test_hasm
+    hbctool asm test_hasm index.android.bundle
+"""
+from docopt import docopt
+import metadata
+import hbc
 import hasm
-import json
+
+def disasm(hbcfile, hasmpath):
+    print(f"[*] Disassemble '{hbcfile}' to '{hasmpath}' path")
+    f = open(hbcfile, "rb")
+    hbco = hbc.load(f)
+    f.close()
+
+    header = hbco.getHeader()
+    sourceHash = bytes(header["sourceHash"]).hex()
+    version = header["version"]
+    print(f"[*] Hermes Bytecode [ Source Hash: {sourceHash}, HBC Version: {version} ]")
+
+    hasm.dump(hbco, hasmpath)
+    print(f"[*] Done")
+
+def asm(hasmpath, hbcfile):
+    print(f"[*] Assemble '{hasmpath}' to '{hbcfile}' path")
+    hbco = hasm.load(hasmpath)
+
+    header = hbco.getHeader()
+    sourceHash = bytes(header["sourceHash"]).hex()
+    version = header["version"]
+    print(f"[*] Hermes Bytecode [ Source Hash: {sourceHash}, HBC Version: {version} ]")
+
+    f = open(hbcfile, "wb")
+    hbc.dump(hbco, f)
+    f.close()
+    print(f"[*] Done")
+
+def main():
+    args = docopt(__doc__, version=f"{metadata.project} {metadata.version}")
+    if args['disasm']:
+        disasm(args['<HBC_FILE>'], args['<HASM_PATH>'])
+    elif args['asm']:
+        asm(args['<HASM_PATH>'], args['<HBC_FILE>'])
+    
+
+def entry_point():
+    """Zero-argument entry point for use with setuptools/distribute."""
+    main()
 
 if __name__ == "__main__":
-    hbc = hbcl.load(open("hbc/hbc74/example/index.android.bundle", "rb"))
-    hbcl.dump(hbc, open("/tmp/index.android.bundle", "wb"))
-    
-    f = open("hbc/hbc74/example/index.android.bundle", "rb")
-    a = f.read()
-    f.close()
-    f = open("/tmp/index.android.bundle", "rb")
-    b = f.read()
-    f.close()
-
-    print(len(a), len(b))
+    main()
---
+++ metadata.py
@@ -0,0 +1,11 @@
+package = 'hbctool'
+project = 'hbctool'
+project_no_spaces = project.replace(' ', '')
+version = '0.1.0'
+description = ('A command-line interface for disassembling and assembling'
+               'the Hermes Bytecode')
+authors = ['bongtrop']
+authors_string = ', '.join(authors)
+emails = ['bongtrop@gmail.com']
+license = 'MIT'
+url = 'https://github.com/bongtrop/hbctool'
