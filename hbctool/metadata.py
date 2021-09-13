e79a802231955fc985ac065d14b7700480906533
bongtrop <bongtrop@gmail.com>
2021-09-13 23:54:45 +0700

    Release version 0.1.4

--- hbctool/metadata.py
+++ hbctool/metadata.py
@@ -1,10 +1,10 @@
 package = 'hbctool'
 project = 'hbctool'
 project_no_spaces = project.replace(' ', '')
-version = '0.1.3'
+version = '0.1.4'
 description = ('A command-line interface for disassembling and assembling'
                'the Hermes Bytecode')
-authors = ['bongtrop']
+authors = ['bongtrop', 'ErbaZZ']
 authors_string = ', '.join(authors)
 emails = ['bongtrop@gmail.com']
 license = 'MIT'
