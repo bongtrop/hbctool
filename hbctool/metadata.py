cd229290e98974bb2d52ca9357f17755c7551c75
bongtrop <bongtrop@gmail.com>
2021-09-17 17:26:06 +0700

    Publish version 0.1.5

--- hbctool/metadata.py
+++ hbctool/metadata.py
@@ -1,11 +1,11 @@
 package = 'hbctool'
 project = 'hbctool'
 project_no_spaces = project.replace(' ', '')
-version = '0.1.4'
+version = '0.1.5'
 description = ('A command-line interface for disassembling and assembling'
                'the Hermes Bytecode')
 authors = ['bongtrop', 'ErbaZZ']
 authors_string = ', '.join(authors)
-emails = ['bongtrop@gmail.com']
+emails = ['bongtrop@gmail.com', 'weerawat.paw@gmail.com']
 license = 'MIT'
 url = 'https://github.com/bongtrop/hbctool'
